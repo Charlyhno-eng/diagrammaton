1d3cdc4e48a91870c71ab0efdff86f6512a3e6d3
Charlyhno-eng <charly7902@gmail.com>
2026-09-12 23:43:11 +0200

    init

---
+++ src/gifenc.d.ts
@@ -0,0 +1,10 @@
+declare module 'gifenc' {
+  export type GifEncoder = {
+    writeFrame: (index: Uint8Array, width: number, height: number, options: { palette: Uint8Array; delay: number; repeat?: number }) => void
+    finish: () => void
+    bytesView: () => Uint8Array
+  }
+  export function GIFEncoder(): GifEncoder
+  export function quantize(pixels: Uint8ClampedArray, maxColors: number): Uint8Array
+  export function applyPalette(pixels: Uint8ClampedArray, palette: Uint8Array): Uint8Array
+}
